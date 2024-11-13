ea6876df0e6a7c286397ed471e67da08378d244f
An <@AneesW03>
2024-11-12 22:24:49 -0400

    Adding PDF generator dependency

--- src/main/java/com/assignmentgrader/Evaluator.java
+++
@@ -1,7 +0,0 @@
-package com.assignmentgrader.app;
-
-import java.util.List;
-
-public interface Evaluator {
-    List<String> runEvaluation(Class<?> chatBotClass);
-}
--- src/main/java/com/assignmentgrader/Extractor.java
+++
@@ -1,7 +0,0 @@
-package com.assignmentgrader.app;
-import java.io.File;
-import java.io.IOException;
-
-public interface Extractor {
-    void extract(String filePath, File destination) throws IOException;
-}
